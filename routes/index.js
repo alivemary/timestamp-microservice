c21676f2b694a57e30b1b6bc9d9e82746b08baa6
alivemary <alivemary@gmail.com>
2017-05-01 22:17:25 +0200

    basic algorithm works

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var moment = require("moment");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res) {
+  res.render("index");
+});
+
+router.get("/:time", function(req, res) {
+  function unixToNatural(unix) {
+    var natural = moment.unix(unix).format("MMMM DD, YYYY");
+    return { unix: unix, time: natural };
+  }
+  function naturalToUnix(natural) {
+    var unix = moment(natural, "MMMM DD, YYYY").format("X");
+    return { unix: unix, time: natural };
+  }
+  if (!isNaN(req.params.time)) {
+    var result = unixToNatural(req.params.time);
+  } else {
+    var result = naturalToUnix(req.params.time);
+  }
+  res.json(result);
 });
 
 module.exports = router;
