6be09dc97a4185c1e2bf865f4d79c7bc4d1ca079
System <none>
2017-05-02 08:27:50 +0000

    🌺👛 Updated with Glitch

--- server.js
+++ server.js
@@ -18,14 +18,25 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/timestamp/", function (req, res) {
+  var data = new Date();
+  var result  = {
+        unix: data.getTime(),
+        time: data.toUTCString()
+      };
+  res.json(result);
+});
+
 app.get("/api/timestamp/:date_string", function (req, res) {
   
   var new_date = req.params.date_string;
   var result = {"error" : "Invalid Date" };
   if (!isNaN(new_date)) {
       new_date *= 1000;
-      }
+  }
+  
   var data = new Date (new_date);
+  
   if (!isNaN(Date.parse(data))) {
       result  = {
         unix: data.getTime(),
@@ -35,13 +46,6 @@ app.get("/api/timestamp/:date_string", function (req, res) {
    
   res.json(result);
 });
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
 
 
 // listen for requests :)
